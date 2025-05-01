b9012f68535f9d361055a4b67c9c5b0a40290f2e
Muhammad AbuBaker <abu.baker@simplifyvms.com>
2025-05-01 16:56:46 +0500

    update .env.example

--- config/services.php
+++ config/services.php
@@ -31,4 +31,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'amadeus' => [
+        'client_id' => env('AMADEUS_CLIENT_ID'),
+        'client_secret' => env('AMADEUS_CLIENT_SECRET'),
+    ]
+
 ];
