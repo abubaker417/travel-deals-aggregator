010187467730f2e2a6bdf97a271456693cafb9de
Muhammad AbuBaker <abu.baker@simplifyvms.com>
2025-05-01 16:45:17 +0500

    created schema for tables

---
+++ database/migrations/2025_04_30_171718_create_deals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deals', function (Blueprint $table) {
+            $table->id();
+            $table->string('type')->nullable();
+            $table->string('origin')->nullable();
+            $table->string('destination')->nullable();
+            $table->decimal('price', 8, 2)->nullable();
+            $table->date('departure_date')->nullable();
+            $table->date('return_date')->nullable();
+            $table->json('details')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deals');
+    }
+};
---
+++ database/migrations/2025_04_30_171920_create_book_marks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book_marks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('deal_id');  
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('deal_id')->references('id')->on('deals')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book_marks');
+    }
+};
